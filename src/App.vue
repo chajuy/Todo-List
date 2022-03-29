3e9a263655a9fe8dd35c9923ea0377fc234d364d
chajuy <juyun2490@gmail.com>
2022-03-30 00:31:26 +0900

    first

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,92 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h2>To-Do List</h2>
+    <form @submit.prevent="onSubmit">
+      <div class="d-flex">
+        <div class="flex-grow-1 mr-2">
+          <input
+            class="form-control"
+            type="text" 
+            v-model="todo"
+            placeholder="Type new to-do"
+          >
+        </div>
+        <div>
+          <button 
+            class="btn btn-primary"
+            type="submit"
+          >
+            Add
+          </button>
+        </div>
+      </div>
+      <div v-show="hasError" style="color: red">
+        This field cannot be empty
+      </div>
+    </form>
+    <div 
+      v-for="todo in todos"
+      :key="todo.id"
+      class="card mt-2"
+    >
+      <div class="card-body p-2">
+        <div class="form-check">
+          <input 
+            class="form-check-input" 
+            type="checkbox"
+            v-model="todo.completed"
+          >
+          <label class="form-check-label" :class="{ todo:todo.completed }">
+            {{ todo.subject }}
+          </label>
+        </div>
+        
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    const todo = ref('');
+    const todos = ref([]);
+    const hasError = ref(false);
+    const todoStyle = {
+      textDecoration: 'line-through',
+      color: 'gray',
+    };
+
+    const onSubmit = () => {
+      if (todo.value === '') {
+        hasError.value = true;
+      } else {
+        todos.value.push({
+          id: Date.now(),
+          subject: todo.value,
+          completed: false,
+        });
+        hasError.value = false;
+        todo.value = '';
+      }
+    };
+
+    return {
+      todo,
+      todos,
+      onSubmit,
+      hasError,
+      todoStyle,
+    };
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .todo {
+    color: gray;
+    text-decoration: line-through;
+  }
 </style>
